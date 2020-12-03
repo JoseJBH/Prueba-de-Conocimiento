40bc5208e3536bcd2cebdf7a7038e7093c6b26c0
JoseJBH <75451037+JoseJBH@users.noreply.github.com>
2020-12-03 17:13:42 -0500

    Add files via upload

---
+++ retobg/src/main/java/com/retobg/RetobgApplication.java
@@ -0,0 +1,15 @@
+package com.retobg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//CLASE QUE ARRANQUE EL PROYECTO; SE GENERA AL CONFIGURAR LA CREACION DEL PROYECTO
+
+@SpringBootApplication
+public class RetobgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetobgApplication.class, args);
+	}
+
+}
---
+++ retobg/src/test/java/com/retobg/RetobgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.retobg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetobgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
